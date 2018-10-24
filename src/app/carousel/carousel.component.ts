1dc432920b28b77002fc3b4b202343b10c78c3eb
SEIJA002 <cisco336@gmail.com>
2018-10-24 12:35:48 -0500

    H

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ContentService } from '../content.service';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-carousel',
@@ -10,41 +11,20 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   encapsulation: ViewEncapsulation.Native
 })
 export class CarouselComponent implements OnInit {
-  contents = [];
-  images = [];
-  selectedIndex: number;
-  transform: number;
+  public contents = [];
   showNavigationArrows = false;
   showNavigationIndicators = false;
   constructor(config: NgbCarouselConfig, private _contentService: ContentService) {
     config.showNavigationArrows = true;
     config.showNavigationIndicators = true;
-    this.selectedIndex = 0;
-    this.transform = 100;
+    this._contentService.getContent().subscribe(content => {
+        this.contents = content;
+        console.log(this.contents);
+        return this.contents;
+      }
+    );
   }
   ngOnInit() {
-    return this._contentService.getContent()
-    .subscribe(content => {
-      this.contents = content;
-      console.log(this.contents);
-    });
+    console.log(this.contents);
   }
-
-  selected(x) {
-    this.downSelected(x);
-    this.selectedIndex = x;
-
-   }
-
-   keySelected(x) {
-     this.downSelected(x);
-     this.selectedIndex = x;
-   }
-   downSelected(i) {
-      // this.transform =  100 - (i) * 50;
-      this.selectedIndex = this.selectedIndex + 1;
-      if (this.selectedIndex > 4) {
-        this.selectedIndex = 0;
-      }
-   }
 }
--- src/app/content.service.ts
+++ src/app/content.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IContent } from './interfaces/content';
 import { Observable } from 'rxjs';
+import { map, filter, scan } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
-  private _url: string = '../assets/mock_data/content.json';
+  private _url: '../assets/mock_data/content.json';
   constructor(private http: HttpClient) { }
 
   getContent(): Observable<IContent[]> {
